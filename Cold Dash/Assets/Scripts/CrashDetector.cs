1d110a3d8ed293d137af47c29187f06e133cb99f
DoğukanKorkmaz <dkorkmaz2000@hotmail.com>
2023-07-17 16:02:26 +0300

    Restart
    Oyuncunun kafasının yere deydiğinde oyunun yeniden başlatılamsı sağlandı.

--- Cold Dash/Assets/Scripts/CrashDetector.cs
+++ Cold Dash/Assets/Scripts/CrashDetector.cs
@@ -1,22 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
-    //void OnTriggerEnter2D(Collider2D other)
-    //{
-    //    if (other.tag == "ground")
-    //    {
-    //        Debug.Log("Ah.. Baþým!");
-    //    }
-        
-    //}
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Ground")
         {
-            Debug.Log(other.gameObject.name);
+            SceneManager.LoadScene(0);  
         }
     }
 }
