d5099d302360e2ea10696677a5282c4274882874
DoğukanKorkmaz <dkorkmaz2000@hotmail.com>
2023-07-17 16:24:38 +0300

    Gecikme ekleme
    Invoke() metodu ekledim.

--- Cold Dash/Assets/Scripts/CrashDetector.cs
+++ Cold Dash/Assets/Scripts/CrashDetector.cs
@@ -5,11 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 1f;
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Ground")
         {
-            SceneManager.LoadScene(0);  
+            Invoke("ReloadScene", loadDelay); 
         }
+    }
+    
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
